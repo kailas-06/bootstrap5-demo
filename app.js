e05a76185e0c489d24854871be1768073f2b3312
Kailas Puri <kailaspuri123@gmail.com>
2022-04-05 13:52:47 +0530

    add design cahnges

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // // Add active class to the current button (highlight it)
-var navbtn = document.getElementById("navbarSupportedContent");
-var navlink = navbtn.getElementsByClassName("nav-item");
-for (var i = 0; i < navlink.length; i++) {
-    navlink[i].addEventListener("click", function() {
-      console.log("Kailas")
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += "active";
-  });
-}
+// var navbtn = document.getElementById("navbarSupportedContent");
+// var navlink = navbtn.getElementsByClassName("nav-item");
+// for (var i = 0; i < navlink.length; i++) {
+//     navlink[i].addEventListener("click", function() {
+//       console.log("Kailas")
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += "active";
+//   });
+// }
